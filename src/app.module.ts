6e419f41f09e414b0198b1409a1246c49539a320
Yestemessov <ruslan.yestemessov@gmail.com>
2022-12-20 20:15:35 +0600

    added main project files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { MessagesModule } from './messages/messages.module';
 
-@Module({})
+@Module({
+  imports: [AuthModule, UsersModule, SubscriptionModule, MessagesModule]
+})
 export class AppModule {}
