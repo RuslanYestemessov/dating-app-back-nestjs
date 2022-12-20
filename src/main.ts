61808700a21db7b72235c9c23323bff263bbd549
Yestemessov <ruslan.yestemessov@gmail.com>
2022-12-20 20:11:49 +0600

    init openAPI (swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('Dating App')
+        .setDescription('Dating App API')
+        .setVersion('v0.1')
+        .addTag('API')
+        .build();
+    await app.listen(3000);
 }
+
 bootstrap();
